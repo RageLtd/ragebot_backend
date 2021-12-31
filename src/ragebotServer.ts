fa885de25aac2388ae40ceec52809c13739761ea
Nathan DeVuono <nathan@devuono.ca>
2021-12-31 00:35:09 -0700

    Add https framesource for Auth0

--- src/ragebotServer.ts
+++ src/ragebotServer.ts
@@ -45,6 +45,7 @@ export function initializeRagebotServer() {
             "https:",
             process.env.NODE_ENV !== "production" ? "unsafe-inline" : "",
           ],
+          frameSrc: ["self", "https:"],
         },
       },
       crossOriginResourcePolicy: {
