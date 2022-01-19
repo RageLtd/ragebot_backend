ec2093f9e1e4461ef4763bf5ee8bcf069b1aeeb4
RageLtd <nathan@devuono.ca>
2022-01-19 16:09:22 -0700

    extreneous import

--- src/frontend/src/views/TriggersView/TriggersView.tsx
+++ src/frontend/src/views/TriggersView/TriggersView.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import Trigger, { iTrigger } from "../../components/Trigger/Trigger";
 import AddNewTriggerForm from "./AddNewTriggerForm/AddNewTriggerForm";
